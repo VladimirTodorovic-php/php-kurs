0f01f94b2008eaf2b79ae0b0b0b7718df9d5d6f8
VladimirTodorovic-php <vladtodorovic95@gmail.com>
2025-07-27 23:20:05 +0200

    Add files via upload

---
+++ varijable.php
@@ -0,0 +1,22 @@
+<?php 
+
+//ovo je tip podatka "STRING"
+// String se uvek pise pod navodnicima
+// String = rec,recenica, slovo, karakter
+
+$mojeIme = "Vladimir";
+
+$godine = 27; // ovo je INTEGER, on se pise bez navonika
+
+$visina = 189.5; //ovo je decimalni zapis, FLOAT
+
+$programer = true; //ovo je BOOLEAN, njegove vrednosti mogu biti true/false (1,0)
+
+echo $mojeIme;
+echo $godine;
+echo $visina;
+echo $programer;
+
+echo "Ja se zovem $mojeIme i imam $godine godina i visok sam $visina";
+
+?>
