9cfd3b823845b1131ca1db7b9517756b7f2c0816
VladimirTodorovic-php <vladtodorovic95@gmail.com>
2025-07-28 18:41:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $ime = "Vladimir";
+    $prezime = "Todorovic";
+    $godina = 30;
+    $visina = 181.2;
+
+    echo "Moje ime je $ime $prezime, i imam $godina godina, a visok sam $visina";
+
+?>
